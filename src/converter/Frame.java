ebacc4015764e6d6173aa479f4b562ef3dde4cd7
Slavica <Slavica@Vladimir>
2015-09-05 17:18:43 +0200

    Prvi komit

---
+++ src/converter/Frame.java
@@ -0,0 +1,23 @@
+package converter;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+    private final static int VISINA = 700;
+    private final static int DUZINA = 500;
+    public static int getVISINA() {
+        return VISINA;
+    }
+    public static int getDUZINA() {
+        return DUZINA;
+    }
+
+    public Frame() throws HeadlessException {
+        setSize(DUZINA, VISINA);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+    }
+}    
---
+++ src/converter/Main.java
@@ -0,0 +1,12 @@
+package converter;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.setVisible(true);
+        frame.pack();
+    }
+    
+}
